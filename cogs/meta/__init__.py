46082c9f8928c285288a83a486d63aa2f212ae57
wompbreaker <wompbreaker@email.com>
2024-11-18 10:00:14 +0100

    Added error handling for setup call

--- cogs/meta/__init__.py
+++ cogs/meta/__init__.py
@@ -1,3 +1,12 @@
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+
 async def setup(bot) -> None:
     from .meta import Meta
-    await bot.add_cog(Meta(bot))
+    try:
+        await bot.add_cog(Meta(bot))
+        logger.info("Meta cog is loaded")
+    except Exception as e:
+        logger.error(f"An error occurred while adding the Meta cog: {e}")

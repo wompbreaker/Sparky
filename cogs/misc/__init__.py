edb33e8a85290c62c0c11a6fce4fa2fba9be57c2
wompbreaker <wompbreaker@email.com>
2024-11-18 10:01:07 +0100

    Added error handling for setup call

--- cogs/misc/__init__.py
+++ cogs/misc/__init__.py
@@ -1,3 +1,11 @@
+from logging import getLogger
+
+logger = getLogger(__name__)
+
 async def setup(bot) -> None:
     from .misc import Misc
-    await bot.add_cog(Misc(bot))
+    try:
+        bot.add_cog(Misc(bot))
+        logger.info("Misc cog is loaded")
+    except Exception as e:
+        logger.error(f"Failed to load Misc cog: {e}")

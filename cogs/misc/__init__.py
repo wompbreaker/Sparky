03538770dcb7eee6ca4966905f3462f449ca376c
wompbreaker <wompbreaker@email.com>
2024-11-18 10:03:32 +0100

    Turned indentation into tabs

--- cogs/misc/__init__.py
+++ cogs/misc/__init__.py
@@ -3,9 +3,9 @@ from logging import getLogger
 logger = getLogger(__name__)
 
 async def setup(bot) -> None:
-    from .misc import Misc
-    try:
-        bot.add_cog(Misc(bot))
-        logger.info("Misc cog is loaded")
-    except Exception as e:
-        logger.error(f"Failed to load Misc cog: {e}")
+	from .misc import Misc
+	try:
+		bot.add_cog(Misc(bot))
+		logger.info("Misc cog is loaded")
+	except Exception as e:
+		logger.error(f"Failed to load Misc cog: {e}")

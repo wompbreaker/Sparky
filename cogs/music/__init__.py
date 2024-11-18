087eeb98714a28a00d15d54928bcc513c032c7f5
wompbreaker <wompbreaker@email.com>
2024-11-18 10:02:08 +0100

    Added error handling for setup call

--- cogs/music/__init__.py
+++ cogs/music/__init__.py
@@ -1,3 +1,11 @@
+from logging import getLogger
+
+logger = getLogger(__name__)
+
 async def setup(bot) -> None:
-    from .music import Music
-    await bot.add_cog(Music(bot))
+	from .music import Music
+	try:
+		await bot.add_cog(Music(bot))
+		logger.info("Music cog is loaded")
+	except Exception as e:
+		logger.error(f"Failed to load Music cog: {e}")

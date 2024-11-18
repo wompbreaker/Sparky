4b76b38e95533959df8cdf08ce06022dc60135ab
wompbreaker <wompbreaker@email.com>
2024-11-18 10:04:26 +0100

    Turned indentation into tabs

--- cogs/fun/__init__.py
+++ cogs/fun/__init__.py
@@ -3,9 +3,9 @@ from logging import getLogger
 logger = getLogger(__name__)
 
 async def setup(bot) -> None:
-    from .fun import Fun
-    try:
-        await bot.add_cog(Fun(bot))
-        logger.info("Fun cog is loaded")
-    except Exception as e:
-        logger.error(f"An error occurred while adding the Fun cog: {e}")
+	from .fun import Fun
+	try:
+		await bot.add_cog(Fun(bot))
+		logger.info("Fun cog is loaded")
+	except Exception as e:
+		logger.error(f"An error occurred while adding the Fun cog: {e}")
--- cogs/meta/__init__.py
+++ cogs/meta/__init__.py
@@ -4,9 +4,9 @@ logger = getLogger(__name__)
 
 
 async def setup(bot) -> None:
-    from .meta import Meta
-    try:
-        await bot.add_cog(Meta(bot))
-        logger.info("Meta cog is loaded")
-    except Exception as e:
-        logger.error(f"An error occurred while adding the Meta cog: {e}")
+	from .meta import Meta
+	try:
+		await bot.add_cog(Meta(bot))
+		logger.info("Meta cog is loaded")
+	except Exception as e:
+		logger.error(f"An error occurred while adding the Meta cog: {e}")

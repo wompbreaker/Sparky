7864fed609294cc8cf3fbf7a9b70c9adaa8c1b45
wompbreaker <wompbreaker@email.com>
2024-11-18 10:02:52 +0100

    Added error handling for setup call

--- cogs/moderation/__init__.py
+++ cogs/moderation/__init__.py
@@ -1,3 +1,11 @@
+from logging import getLogger
+
+logger = getLogger(__name__)
+
 async def setup(bot) -> None:
-    from .moderation import Moderation
-    await bot.add_cog(Moderation(bot))
+	from .moderation import Moderation
+	try:
+		await bot.add_cog(Moderation(bot))
+		logger.info("Moderation cog is loaded")
+	except Exception as e:
+		logger.error(f"Failed to load Moderation cog: {e}")

b1b8f6e918d4f3dbcb54c74219684c2930620a22
wompbreaker <wompbreaker@email.com>
2024-11-18 10:14:28 +0100

    Awaited bot.add_cog

--- cogs/misc/__init__.py
+++ cogs/misc/__init__.py
@@ -5,7 +5,7 @@ logger = getLogger(__name__)
 async def setup(bot) -> None:
 	from .misc import Misc
 	try:
-		bot.add_cog(Misc(bot))
+		await bot.add_cog(Misc(bot))
 		logger.info("Misc cog is loaded")
 	except Exception as e:
 		logger.error(f"Failed to load Misc cog: {e}")

8b7974d8d01f7e6d64f18170f896e02a848b245c
wompbreaker <wompbreaker@email.com>
2024-11-18 09:58:28 +0100

    Added error handling for setup call

--- cogs/fun/__init__.py
+++ cogs/fun/__init__.py
@@ -1,3 +1,11 @@
+from logging import getLogger
+
+logger = getLogger(__name__)
+
 async def setup(bot) -> None:
     from .fun import Fun
-    await bot.add_cog(Fun(bot))
+    try:
+        await bot.add_cog(Fun(bot))
+        logger.info("Fun cog is loaded")
+    except Exception as e:
+        logger.error(f"An error occurred while adding the Fun cog: {e}")
